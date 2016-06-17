6c74cc729ec3915237b0562f4e664e17f2766226
Tom Payne <twpayne@gmail.com>
2016-06-17 02:37:59 +0200

    Add example

---
+++ vali_test.go
@@ -0,0 +1,25 @@
+package vali
+
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/net/context"
+)
+
+func ExampleService_ValidateIGC() {
+	filename := "testdata/2006-06-10-XXX-3XI-01.IGC"
+	igcFile, err := os.Open(filename)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer igcFile.Close()
+	ctx := context.Background()
+	if ok, err := NewService().ValidateIGC(ctx, filename, igcFile); !ok {
+		fmt.Println(err)
+	} else {
+		fmt.Println("OK")
+	}
+	// Output: OK
+}
